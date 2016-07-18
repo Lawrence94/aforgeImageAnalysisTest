8fd8f14ffda2b0a066d2eb3f7403b73ed614e06b
Lawrence Agbani <L.agbani@yahoo.com>
2016-07-18 17:54:39 +0100

    Initial commit

---
+++ ImageAnalysisTest/Form1.Designer.cs
@@ -0,0 +1,193 @@
+﻿namespace ImageAnalysisTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.processingToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.grayscaleImageToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.blackAndWhiteToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.blackBackgroundToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.countCellsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.processingToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(900, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.pictureBox1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.pictureBox2, 1, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 24);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(900, 407);
+            this.tableLayoutPanel1.TabIndex = 1;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(3, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(444, 401);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox2.Location = new System.Drawing.Point(453, 3);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(444, 401);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // processingToolStripMenuItem
+            // 
+            this.processingToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.grayscaleImageToolStripMenuItem,
+            this.blackAndWhiteToolStripMenuItem,
+            this.blackBackgroundToolStripMenuItem,
+            this.countCellsToolStripMenuItem});
+            this.processingToolStripMenuItem.Name = "processingToolStripMenuItem";
+            this.processingToolStripMenuItem.Size = new System.Drawing.Size(76, 20);
+            this.processingToolStripMenuItem.Text = "Processing";
+            // 
+            // grayscaleImageToolStripMenuItem
+            // 
+            this.grayscaleImageToolStripMenuItem.Name = "grayscaleImageToolStripMenuItem";
+            this.grayscaleImageToolStripMenuItem.Size = new System.Drawing.Size(160, 22);
+            this.grayscaleImageToolStripMenuItem.Text = "Grayscale Image";
+            this.grayscaleImageToolStripMenuItem.Click += new System.EventHandler(this.grayscaleImageToolStripMenuItem_Click);
+            // 
+            // blackAndWhiteToolStripMenuItem
+            // 
+            this.blackAndWhiteToolStripMenuItem.Name = "blackAndWhiteToolStripMenuItem";
+            this.blackAndWhiteToolStripMenuItem.Size = new System.Drawing.Size(160, 22);
+            this.blackAndWhiteToolStripMenuItem.Text = "Black and white";
+            this.blackAndWhiteToolStripMenuItem.Click += new System.EventHandler(this.blackAndWhiteToolStripMenuItem_Click);
+            // 
+            // blackBackgroundToolStripMenuItem
+            // 
+            this.blackBackgroundToolStripMenuItem.Name = "blackBackgroundToolStripMenuItem";
+            this.blackBackgroundToolStripMenuItem.Size = new System.Drawing.Size(169, 22);
+            this.blackBackgroundToolStripMenuItem.Text = "Black background";
+            this.blackBackgroundToolStripMenuItem.Click += new System.EventHandler(this.blackBackgroundToolStripMenuItem_Click);
+            // 
+            // countCellsToolStripMenuItem
+            // 
+            this.countCellsToolStripMenuItem.Name = "countCellsToolStripMenuItem";
+            this.countCellsToolStripMenuItem.Size = new System.Drawing.Size(169, 22);
+            this.countCellsToolStripMenuItem.Text = "Count Cells";
+            this.countCellsToolStripMenuItem.Click += new System.EventHandler(this.countCellsToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(900, 431);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.ToolStripMenuItem processingToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem grayscaleImageToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem blackAndWhiteToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem blackBackgroundToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem countCellsToolStripMenuItem;
+    }
+}
+
---
+++ ImageAnalysisTest/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AForge;
+using AForge.Imaging;
+using AForge.Imaging.ColorReduction;
+using AForge.Imaging.ComplexFilters;
+using AForge.Imaging.Filters;
+using AForge.Math.Geometry;
+
+namespace ImageAnalysisTest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                pictureBox1.Image = (Bitmap)System.Drawing.Image.FromFile(openFileDialog1.FileName);
+                 
+            }
+        }
+
+        private void grayscaleImageToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            
+            GrayscaleBT709 gray = new GrayscaleBT709();
+            pictureBox2.Image = gray.Apply((Bitmap)pictureBox1.Image);
+        }
+
+        private void blackAndWhiteToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            EuclideanColorFiltering filter = new EuclideanColorFiltering();
+            filter.CenterColor = new AForge.Imaging.RGB(Color.White); //Pure White
+            filter.Radius = 40; //Increase this to allow off-whites
+            filter.FillColor = new AForge.Imaging.RGB(Color.Red); //Replacement Colour
+            //filter.ApplyInPlace(image);
+            // apply the filter
+            //filter.ApplyInPlace(image);
+            Bitmap newImage = filter.Apply((Bitmap)pictureBox1.Image);
+            
+            pictureBox2.Image = newImage;
+        }
+
+        private void blackBackgroundToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // create filter
+            ColorFiltering filter = new ColorFiltering();
+            // set color ranges to keep
+            filter.Red = new IntRange(100, 255);
+            filter.Green = new IntRange(0, 75);
+            filter.Blue = new IntRange(0, 75);
+            // apply the filter
+            // filter.ApplyInPlace((Bitmap)pictureBox2.Image);
+            Bitmap newImage = filter.Apply((Bitmap)pictureBox2.Image);
+            pictureBox2.Image = newImage;
+        }
+
+        private void countCellsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            BlobCounter blobCounter = new BlobCounter();
+            blobCounter.ProcessImage((Bitmap)pictureBox2.Image);
+            Blob[] blobs = blobCounter.GetObjectsInformation();
+            System.Diagnostics.Debug.WriteLine("Objects count: " + blobs.Length);
+            //// create Graphics object to draw on the image and a pen
+            //Graphics g = Graphics.FromImage(bitmap);
+            //Pen bluePen = new Pen(Color.Blue, 2);
+            //// check each object and draw circle around objects, which
+            //// are recognized as circles
+            //for (int i = 0, n = blobs.Length; i < n; i++)
+            //{
+            //    List<IntPoint> edgePoints = blobCounter.GetBlobsEdgePoints(blobs[i]);
+            //    List<IntPoint> corners = PointsCloud.FindQuadrilateralCorners(edgePoints);
+
+            //    g.DrawPolygon(bluePen, ToPointsArray(corners));
+            //}
+
+            //bluePen.Dispose();
+            //g.Dispose();
+        }
+    }
+}
